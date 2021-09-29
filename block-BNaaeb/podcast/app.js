a0ab607f901caef63f831be5da6db1872f415e2c
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-09-29 15:46:24 +0530

    update

--- block-BNaaeb/podcast/app.js
+++ block-BNaaeb/podcast/app.js
@@ -3,9 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
+var flash = require('connect-flash');
+
+// require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
+var clientRouter = require('./routes/client');
+var homeRouter = require('./routes/home');
+var podcastRouter = require('./routes/podcast');
+var auth = require('./middlewares/auth');
+
+// Connect with database
+mongoose.connect('mongodb://localhost/podcast', (err) => {
+  console.log(err ? err : 'Connected to DB.');
+});
+
 
 var app = express();
 
@@ -19,16 +36,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Add Sessions
+
+app.use(
+  session({
+    secret: "somesecretmessage",
+    saveUninitialized: false,
+    resave: false,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/podcast' }),
+  })
+);
+
+
+app.use(flash());
+
+app.use(auth.userInfo);
+// app.use(auth.urlInfo);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/home', homeRouter);
+app.use('/client', clientRouter);
+app.use('/admin', adminRouter);
+app.use('/podcast', podcastRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- block-BNaaeb/podcast/routes/users.js
+++ block-BNaaeb/podcast/routes/users.js
@@ -1,9 +1,81 @@
 var express = require('express');
+var User = require('../models/User');
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+// register
+
+router.get('/register', function (req, res, next) {
+  res.render('register', { error: req.flash('error')[0] });
+});
+
+router.post('/register', (req, res, next) => {
+  User.create(req.body, (err, user) => {
+    if (err) {
+      if (err.name === 'ValidationError') {
+        req.flash('error', err.message);
+        return res.redirect('/users/register');
+      }
+      req.flash('error', 'This email is taken');
+      return res.redirect('/users/register');
+      // return res.json({ err });
+    }
+
+    res.redirect('/users/login');
+  });
+});
+
+// Login
+
+router.get('/login', (req, res, next) => {
+  var error = req.flash('error')[0];
+  res.render('login', { error });
+});
+
+router.post('/login', (req, res, next) => {
+  var { email, password } = req.body;
+  if (!email || !password) {
+    req.flash('error', 'Email/Password required');
+    return res.redirect('/users/login');
+  }
+  User.findOne({ email }, (err, user) => {
+    if (err) return next(err);
+    // no user
+    if (!user) {
+      req.flash('error', 'This email is not registered');
+      return res.redirect('/users/login');
+    }
+    // compare password
+    user.verifyPassword(password, (err, result) => {
+      if (err) return next(err);
+      if (!result) {
+        req.flash('error', 'Invalid Password');
+        return res.redirect('/users/login');
+      }
+      // persist login user info
+      req.session.userId = user.id;
+      req.session.userType = user.userType;
+      res.redirect('/home');
+    });
+  });
+});
+
+// Logout
+
+router.get('/logout', (req, res, next) => {
+  if (!req.session) {
+    req.flash('error', 'You must login first');
+    res.redirect('/users/login');
+  } else {
+    req.session.destroy();
+    res.clearCookie('connect.sid');
+    res.redirect('/users/login');
+  }
 });
 
 module.exports = router;
